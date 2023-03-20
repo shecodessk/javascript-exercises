57855dc7c23474566f1d3a096519afefbd08ca15
Stephany <stephanykleen@gmail.com>
2023-03-20 15:06:07 -0700

    Fix exercise 03 bug - Reverse String
    Exercise needs to reverse a string and return the new reversed value

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let myArray = string.split("").reverse().join("");
+  return myArray
 };
 
 // Do not edit below this line
