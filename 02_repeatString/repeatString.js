e00a0239b5f7e9ba5c334b89e549da388eafdd2b
Stephany <stephanykleen@gmail.com>
2023-03-20 14:12:25 -0700

    Fix exercise 02 bug - Repeat String
    Exercise needs to repeat string as many times as "number" argument
    
    Edge case for 0, and for negative numbers.
    
    Resolved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+  if(number < 0){
+    return 'ERROR'
+  }
+  let repeatedString = '';
+  while( number > 0){
+   repeatedString += string;
+    --number;
+  }
+  return repeatedString
 };
 
 // Do not edit below this line
