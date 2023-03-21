17c9b768341ca64d88bd4fbf443fbfdbe620438b
Stephany <stephanykleen@gmail.com>
2023-03-21 11:06:02 -0700

    exercise 05 - sumAll parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a , b) {
+  if (a < 0 || b < 0){return 'ERROR'}
+  if(typeof a !== "number" || typeof b !== "number"){return 'ERROR'}
+  if (a > b){
+    const exchangeParameters = a
+    a = b
+    b = exchangeParameters
+  }
+  let add = 0;
+  for(i = a; i <= b; i++){
+    add += i 
+  }
+    return add
+  }
 
 // Do not edit below this line
 module.exports = sumAll;
